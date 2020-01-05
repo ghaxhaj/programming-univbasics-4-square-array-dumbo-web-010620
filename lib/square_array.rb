8cb9ab06a99f68b58dae19a3c3a6dad9a91267c2
wise-integer-6834 <ghaxhaj@fordham.edu>
2020-01-05 20:29:53 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,7 +1,7 @@
-def square_array(array)
+def square_array(numbers)
   counter = 0
-  while array[counter] do
-    puts array[counter]**2
+  while numbers[counter] do
+    puts numbers[counter]**2
     counter +=1
   end
 end
