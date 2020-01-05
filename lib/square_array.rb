2c6f23fe27085ee702b1b39eead8c5b1c9f80748
wise-integer-6834 <ghaxhaj@fordham.edu>
2020-01-05 20:26:57 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,12 +1,2 @@
-def square_array(array)
-  squared = []
-  count = 0
-  while count < array.size do
-    squared << (array[count] ** 2)
-    count += 1
-  end
-  squared
+def square_array
 end
-
-array = [1,2,3]
-square_array(array)
